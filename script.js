40167070aef2ed7159c4b0236187747537533ea9
peterrahme <peter.rahme@ocsbstudent.ca>
2021-02-10 21:27:18 +0000

    javascript

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // changes the display of text and color of the background
 function buttonOn () {
-  document.getElementById("me").innerHTML = "Ok"
-  document.getElementById("background").style.backgroundColor = "red"
+  document.getElementById('me').innerHTML = 'Ok'
+  document.getElementById('background').style.backgroundColor = 'red'
 }
 
 // changes display of images and text
 function goatbuttonOn () {
-  document.getElementById("picture").src = "./Images/Goat2.jpg"
-  document.getElementById("picture").alt = "Lebron James"
-  document.getElementById("x").style.display = "none"
+  document.getElementById('picture').src = './Images/Goat2.jpg'
+  document.getElementById('picture').alt = 'Lebron James'
+  document.getElementById('x').style.display = 'none'
 }
