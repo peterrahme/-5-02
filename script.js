7fdea13a1aa561cd4023612a84e0c58b40beb498
peterrahme <peter.rahme@ocsbstudent.ca>
2021-02-10 21:19:22 +0000

    js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//changes the display of text and color of the background
+// changes the display of text and color of the background
 function buttonOn () {
   document.getElementById("me").innerHTML = "Ok"
   document.getElementById("background").style.backgroundColor = "red"
 }
 
-//changes display of images and text
+// changes display of images and text
 function goatbuttonOn () {
   document.getElementById("picture").src = "./Images/Goat2.jpg"
   document.getElementById("picture").alt = "Lebron James"
